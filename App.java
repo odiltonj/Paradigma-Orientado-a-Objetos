504459fd0875dc6059e7acb69692248c0d88fa42
odiltonjr <odiltonjr@live.com>
2025-05-28 19:06:43 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,12 @@
 public class App {
     public static void main(String[] args){
 
-        Funcionario f1 = new Funcionario("Joao", 1000, "05798358550");
-        Funcionario f2 = new Funcionario("Maria", 200, "12345678900");
+        Funcionario f1 = new Funcionario("Joao", 1000);
+        Funcionario f2 = new Funcionario("Maria", 200);
 
 
         System.out.println("nome: "+f1.nome);
         System.out.println("salario: "+f1.salario);
-        System.out.println("cpf: "+f1.cpf);
 
         System.out.println(f2);
         
