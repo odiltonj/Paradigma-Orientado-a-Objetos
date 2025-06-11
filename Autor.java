3769908aea3642a7547df0d46514f4bf4b2633af
odiltonjr <odiltonjr@live.com>
2025-06-11 19:49:12 -0300

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -1,15 +1,25 @@
 public class Autor {
-    private String nome;
+    String nome;
+    String cpf;
+    String cidade;
 
-    public Autor(String nome) {
+    public Autor(String nome, String cpf, String cidade) {
         this.nome = nome;
+        this.cpf = cpf;
+        this.cidade = cidade;
     }
 
+    //gets e sets dos atributos.
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
+    
+    //métodos dessa classe Autor
+    public escreveLivro(){
+        System.out.println("O Autor escreveu este livro");
+    }
+
 }
