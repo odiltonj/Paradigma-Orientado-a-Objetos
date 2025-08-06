fddf7abc742a87fba2cdb2ed7d32c6d6261b0aef
odiltonjr <odiltonjr@live.com>
2025-08-05 23:17:07 -0300

    Add files via upload

--- Autor.java
+++ Autor.java
@@ -1,25 +1,15 @@
 public class Autor {
-    String nome;
-    String cpf;
-    String cidade;
+    private String nome;
 
-    public Autor(String nome, String cpf, String cidade) {
+    public Autor(String nome) {
         this.nome = nome;
-        this.cpf = cpf;
-        this.cidade = cidade;
     }
 
-    //gets e sets dos atributos.
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    //métodos dessa classe Autor
-    public escreveLivro(){
-        System.out.println("O Autor escreveu este livro");
-    }
-
 }
